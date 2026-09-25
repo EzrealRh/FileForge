81cb9704b45aade0852abed9f7ccc549fac1b1a7
EzrealRh <EzrealRh@users.noreply.github.com>
2026-09-25 15:20:38 +0800

    版本 0.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-val appVersion = "0.6.2"
+val appVersion = "0.6.3"
 
 plugins {
     alias(libs.plugins.android.application)
@@ -14,7 +14,7 @@ android {
         applicationId = "com.fileforge.converter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
+        versionCode = 20
         versionName = appVersion
         resourceConfigurations += listOf("zh", "en")
     }
